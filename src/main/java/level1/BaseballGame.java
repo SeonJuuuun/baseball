daea4707a6028c6d1cf3fee1807684a4b42e69ad
yuseonjun <seonjun0906@gmail.com>
2024-09-15 18:50:39 +0900

    [Feat] Level1 repeat 메서드를 통해 올바르지 않은 입력 시 에러 메세지를 출력한 후 그 부분부터 다시 시작 구현

--- src/main/java/level1/BaseballGame.java
+++ src/main/java/level1/BaseballGame.java
@@ -1,29 +1,63 @@
 package level1;
 
 import java.util.Arrays;
+import java.util.function.Supplier;
 import level1.domain.Computer;
+import level1.domain.Hint;
 import level1.domain.NumberGenerator;
 import level1.domain.Player;
+import level1.domain.Referee;
+import level1.exception.BaseballException;
 import level1.view.Input;
 import level1.view.Output;
 
 public class BaseballGame {
 
     private final NumberGenerator numberGenerator;
+    private final Referee referee;
 
-    public BaseballGame(NumberGenerator numberGenerator) {
+    public BaseballGame(final NumberGenerator numberGenerator, final Referee referee) {
         this.numberGenerator = numberGenerator;
+        this.referee = referee;
     }
 
-    public Computer settingComputerNumber() {
+    public void start() {
+        Output.printStartMessage();
+        final Computer computer = settingComputerNumber();
+        play(computer);
+    }
+
+    private void play(final Computer computer) {
+        while (true) {
+            final Player player = repeat(this::inputPlayerNumber);
+            final Hint hint = referee.judge(computer, player);
+            Output.printResult(hint.result());
+            if (hint.isThreeStrike()) {
+                Output.printAnswerMessage();
+                break;
+            }
+            Output.printSpace();
+        }
+    }
+
+    private Computer settingComputerNumber() {
         return new Computer(numberGenerator);
     }
 
-    public Player inputPlayerNumber() {
+    private Player inputPlayerNumber() {
         Output.requestPlayerNumber();
         final String playerNumber = Input.readPlayerNumber();
         return new Player(Arrays.stream(playerNumber.split(""))
                 .map(Integer::parseInt)
                 .toList());
+    }
+
+    private <T> T repeat(Supplier<T> inputReader) {
+        try {
+            return inputReader.get();
+        } catch (BaseballException e) {
+            System.out.println(e.getMessage());
+            return repeat(inputReader);
+        }
     }
 }
