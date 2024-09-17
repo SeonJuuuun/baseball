f697593fa9a989750819a786c7cc9e575b83e978
yuseonjun <seonjun0906@gmail.com>
2024-09-17 14:37:59 +0900

    [Feat] Level1 힌트 판별 기능 간결하게 개선

--- src/main/java/level1/view/Output.java
+++ src/main/java/level1/view/Output.java
@@ -1,5 +1,7 @@
 package level1.view;
 
+import level1.domain.Hint;
+
 public class Output {
 
     public static void printStartMessage() {
@@ -10,13 +12,32 @@ public class Output {
         System.out.println("숫자를 입력하세요");
     }
 
-    public static void printResult(final String result) {
-        System.out.println(result);
+    public static void printResult(final Hint hint) {
+        final int strikeCount = hint.getStrikeCount();
+        final int ballCount = hint.getBallCount();
+        final boolean ended = hint.isEnded();
+
+        if (ended) {
+            System.out.println("정답입니다!");
+            return;
+        }
+
+        if (strikeCount == 0 && ballCount == 0) {
+            System.out.println("아웃");
+            return;
+        }
+
+        StringBuilder result = new StringBuilder();
+        if (strikeCount > 0) {
+            result.append(strikeCount).append("스트라이크 ");
+        }
+        if (ballCount > 0) {
+            result.append(ballCount).append("볼");
+        }
+
+        System.out.println(result.toString().trim());
     }
 
-    public static void printAnswerMessage() {
-        System.out.println("정답입니다 !");
-    }
 
     public static void printSpace() {
         System.out.println();
